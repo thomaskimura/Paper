5af8752b02560e913f117b4f4464794ee6c4bfd4
Thomas Kimura <thomas.kimura@ymail.com>
2014-04-10 23:53:00 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,73 +1,76 @@
-
-    $('.photoset-grid').photosetGrid({
-  highresLinks: true,
-  rel: $('.photoset-grid').attr('data-id'),
-  gutter: '5px',
-
-  onComplete: function(){});
-  }
+$(document).ready(function(){
+    
+    //PHOTOSETS
+   $('.photo-slideshow').pxuPhotoset({
+        // options go here
+        'highRes'   : true, // will use high res images
+        'rounded'   : 'false', // corners, all or false
+        'exif'      : false, // display EXIF data if available (tooltip)
+        'captions'  : false, // display individual captions on photos (tooltip)
+        'gutter'    : '10px', // spacing between the images
+        'photoset'  : '.photo-slideshow', // photoset wrapper
+        'photoWrap' : '.photo-data', // photo data wrapper (includes photo, icons + exif)
+        'photo'     : '.pxu-photo' // photo wrap (includes photo only)
+    }, function() {
+        // callback
+    });
+    
 });
+//end ready
 
+$.slidebars();
 
+$(window).load(function() {
+   //alert("window is loaded");
+   document.body.focus();
+});
+//end load
 
-    //Ask button
-    function askOn()
-    {
-        if( $('#ask').css('display') == "block" )  //check if as is displayed
-        { 
-            //ask is displayed 
-            document.getElementById("ask").style.display = 'none';
-        } 
-        else 
-        { 
-            //ask is not displayed 
-            document.getElementById("ask").style.display = 'block';
-            //call scroll function
-            scroll();
-            document.getElementById("submit").style.display = 'none';
+//GLOBAL
+function askOn(){
+    if( $('#ask').css('display') == "block" ){
+        document.getElementById("ask").style.display = 'none';} 
+    else { 
+        document.getElementById("ask").style.display = 'block';
+        scroll();
+        document.getElementById("submit").style.display = 'none';}
+}
+
+//SUBMIT
+function submitOn(){
+    if( $('#submit').css('display') == "block" ) {
+        document.getElementById("submit").style.display = 'none';} 
+    else { 
+        document.getElementById("submit").style.display = 'block';
+        scroll();
+        document.getElementById("ask").style.display = 'none';}
+}
+
+//SCROLL TO
+function scroll() {
+    $('html, body').animate({scrollTop: $(".asksubmit").offset().top}, 500);
+        window.location.hash="content"; //FALL BACK
+}
+</script>
+
+<!--KEYBOARD NAV ON INDEX-->
+{block:IndexPage}
+<script>
+  //RANDOM NAVIGATION FOR INDEX
+  document.body.onkeydown = function(e){   
+        if (e == null) {
+            keycode = event.keyCode;
+        } else {
+            keycode = e.which;
         }
-    }
-    
-    //Submit button
-    function submitOn()
-    {
-        if( $('#submit').css('display') == "block" )  //check if as is displayed
-        { 
-            //ask is displayed 
-            document.getElementById("submit").style.display = 'none';
+        if(keycode == 80){
+            $("#sb-site").fadeOut();
+            //alert('Random');
+            setTimeout(function(){
+                window.location = $('#random').attr('href');
+            },100);
         } 
-        else 
-        { 
-            //ask is not displayed 
-            document.getElementById("submit").style.display = 'block';
-            //call scroll function
-            scroll();
-            document.getElementById("ask").style.display = 'none';
+         if (url) {
+        window.location = url;
         }
-    }
-    
-    //scroll to ask/submit box
-    function scroll() {
-        $('html, body').animate({
-        scrollTop: $(".asksubmit").offset().top
-    }, 500);
-    }
-    
-
-
-
-    $.SyntaxHighlighter.init({
-        'wrapLines':false
-    });
-
-
-
-
-
-    var userFeed = new Instafeed({
-        limit: 20,
-        get: 'user',
-        userId: {text:Instagram User ID},
-        accessToken: '{text:Instagram Access Token}',
-    });
-    userFeed.run();
+    };
